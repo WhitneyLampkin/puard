46c97ff3d667b03a40f5f019ce6d6a2fda68c5bc
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-07-18 13:48:44 -0500

    complete setup for c3 working successfully

--- chapter-3/hello-world.py
+++ chapter-3/hello-world.py
@@ -1,10 +1,11 @@
 # Install Python and Prometheus Client:
-    # sudo apt install python3
-    # sudo apt install python3-pip
-    # pip3 install prometheus_client
+# sudo apt install python3
+# sudo apt install python3-pip
+# pip3 install prometheus_client
 
 import http.server
 from prometheus_client import start_http_server
+
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -12,8 +13,10 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Hello World")
 
+
 if __name__ == "__main__":
     start_http_server(8000)
     # Changed http server and port from 'localhost' and '8001' to work wtih docker
+    # server = http.server.HTTPServer(('0.0.0.0', 80), MyHandler)
     server = http.server.HTTPServer(('0.0.0.0', 80), MyHandler)
     server.serve_forever()
