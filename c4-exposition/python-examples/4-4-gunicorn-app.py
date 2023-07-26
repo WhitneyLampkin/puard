9964cb63d36e13810a50b8a7a501aea1a91c34fe
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-07-26 10:53:02 -0500

    more c4 examples

---
+++ c4-exposition/python-examples/4-4-gunicorn-app.py
@@ -0,0 +1,22 @@
+# [!!IMPT] Gunicorn requires that the prometheus_multiproc_dir be setup (empty directory).
+
+from prometheus_client import multiprocess, make_wsgi_app, CollectorRegistry
+from prometheus_client import Counter, Gauge
+
+REQUESTS = Counter("http_requests_total", "HTTP requests")
+# For Gunicorn apps, guage metrics require an additional optional configuration (multiprocess_mode).
+# All other metrics are configured the same...
+IN_PROGRESS = Gauge("http_requests_inprogress", "Inprogress HTTP requests",
+                    multiprocess_mode='livesum')
+
+
+@IN_PROGRESS.track_inprogress()
+def app(environ, start_fn):
+    REQUESTS.inc()
+    if environ['PATH_INFO'] == '/metrics':
+        registry = CollectorRegistry()
+        multiprocess.MultiProcessCollector(registry)
+        metrics_app = make_wsgi_app(registry)
+        return metrics_app(environ, start_fn)
+    start_fn('200 OK', [])
+    return [b'Hello World']
